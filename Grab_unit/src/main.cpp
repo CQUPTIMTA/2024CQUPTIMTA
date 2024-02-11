41c1f028d9d6e2388f95a6d8a761386a5392e3d0
qingmeijiupiao <2368357055@qq.com>
2024-02-11 22:43:51 +0800

    添加了随机摆放位置生成器

--- Grab_unit/src/main.cpp
+++ Grab_unit/src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
 #include "ESP32FLASHEEPROM.hpp"
-ESP32FLASHEEPROM DATA();
-
+ESP32FLASHEEPROM DATA;
+#include "SENSOR.hpp"
 
 void setup() {
-  DATA().setup();
-  DATA().read();
+  DATA.setup();
+  DATA.read();
 }
 
 void loop() {
--- recognition_unit/src/main.cpp
+++ recognition_unit/src/main.cpp
@@ -1,18 +1,18 @@
 #include <Arduino.h>
 #include "ESP32FLASHEEPROM.hpp"
-ESP32FLASHEEPROM flash_data();
+ESP32FLASHEEPROM flash_data;
 #include "SENSOR.hpp"
 SENSOR font_sensor(15, 2);
 SENSOR back_sensor(15, 2);
 
 void setup() {
-  flash_data.setup();
-  flash_data.read();
+  flash_data.setup();//初始化
+  flash_data.read();//读取
   font_sensor.setup();
   back_sensor.setup();
+  delay(1000);
 }
 
 void loop() {
-
 }
 
